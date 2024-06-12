d1d62a04d29c4314b171d237adba703562841d4a
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2024-06-12 21:32:15 +0100

    + html and javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+const button = document.querySelector("button");
+const url = 'https://bored.api.lewagon.com/api/activity';
+button.addEventListener("click", () => { 
+    const results = document.querySelector("#results")
+    const activity = document.querySelector(".activity")
+    const price = document.querySelector(".price")
+    const people = document.querySelector(".participants")
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        results.classList.remove("invisible")
+        activity.innerHTML = data.activity;
+        if (data.participants==1){
+            people.innerHTML = "you can do this by yourslef!"
+        }
+        else{
+            people.innerHTML = "you can do this with " + data.participants + " people"
+        }
+        price.innerHTML =data.price + "$"
+    })
+    .catch(error => console.error('There was a problem with the fetch operation:', error));
+})
